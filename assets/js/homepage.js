76d8e814980f38f422b920578514ca583eacf677
jypara-git <zhyparaabdieva@gmail.com>
2021-03-10 14:40:43 -0800

    create a link between pages

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -39,8 +39,7 @@ var formSubmitHandler = function(event) {
 }
 userFormEl.addEventListener("submit", formSubmitHandler);
 var displayRepos = function(repos, searchTerm) {
-    console.log(repos);
-    console.log(searchTerm);
+
     // check if api returned any repos
     if (repos.length === 0) {
         repoContainerEl.textContent = "No repositories found.";
@@ -55,8 +54,9 @@ var displayRepos = function(repos, searchTerm) {
         var repoName = repos[i].owner.login + "/" + repos[i].name;
 
         // create a container for each repo
-        var repoEl = document.createElement("div");
+        var repoEl = document.createElement("a");
         repoEl.classList = "list-item flex-row justify-space-between align-center";
+        repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
 
         // create a span element to hold repository name
         var titleEl = document.createElement("span");
