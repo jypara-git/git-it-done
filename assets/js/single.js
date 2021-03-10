3d130d9635d73136a8a395d27ae1f12817c54f7b
jypara-git <zhyparaabdieva@gmail.com>
2021-03-10 01:09:07 -0800

    add link to see more than 30 issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 var getRepoIssues = function(repo){
     console.log(repo);
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -6,21 +7,25 @@ var getRepoIssues = function(repo){
         // request was successful 
         if (response.ok) {
             response.json().then(function(data) {
-                // pass response data to dom function
                 displayIssues(data);
-            });
-        } 
+                // check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
+            })
+        }
         else {
             alert("There was a problem with your request!");
         }
     });
 };
-getRepoIssues("microsoft/3-in-1-dock");
+getRepoIssues("facebook/buck");
 var displayIssues = function(issues) {
     if (issues.length === 0) {
         issueContainerEl.textContent = "This repo has no open issues.";
         return;
     }
+
     for (var i = 0; i < issues.length; i++) {
         // create a link element to take users to the issue on github
         var issueEl = document.createElement("a");
@@ -47,4 +52,15 @@ var displayIssues = function(issues) {
         issueEl.appendChild(typeEl);
         issueContainerEl.appendChild(issueEl);
     }
+};
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit";
+    var linkEl = document.createElement("a");
+    linkEl.textContent = " See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
 };
