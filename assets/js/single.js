6c5613364df32b012cb367c812164716ecffeba2
jypara-git <zhyparaabdieva@gmail.com>
2021-03-11 00:27:16 -0800

    add error handling

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,25 +1,43 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
-var getRepoIssues = function(repo){
-    console.log(repo);
-    var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+var repoNameEl = document.querySelector("#repo-name");
+var getRepoName = function(){
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+
+    if (repoName) {
+        // display repo name on the page
+        repoNameEl.textContent = repoName;
+
+        getRepoIssues(repoName);
+    } else {
+        // if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    } 
+};
+var getRepoIssues = function(repoName){
+    
+    var apiUrl = "https://api.github.com/repos/" + repoName + "/issues?direction=asc";
+    // make a get request to url
     fetch(apiUrl).then(function(response) {
         // request was successful 
         if (response.ok) {
-            response.json().then(function(data) {
-                displayIssues(data);
+            response.json().then(function(repoName) {
+                displayIssues(repoName);
                 // check if api has paginated issues
                 if (response.headers.get("Link")) {
-                    displayWarning(repo);
+                    displayWarning(repoName);
                 }
             })
         }
         else {
-            alert("There was a problem with your request!");
+            // if not successful redirect to homepage
+            document.location.replace("./index.html");
         }
     });
 };
-getRepoIssues("facebook/buck");
+// getRepoIssues();
 var displayIssues = function(issues) {
     if (issues.length === 0) {
         issueContainerEl.textContent = "This repo has no open issues.";
@@ -64,3 +82,4 @@ var displayWarning = function(repo) {
     // append to warning container
     limitWarningEl.appendChild(linkEl);
 };
+getRepoName();
